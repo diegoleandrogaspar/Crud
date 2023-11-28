bfa9cf6d676b56b8df724797c75a16154489ed67
diegoleandrogaspar <diegolgaspar000@gmail.com>
2023-11-28 17:55:02 -0300

    feat: add DELETE and PUT

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -3,10 +3,13 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -15,8 +18,7 @@ public class ProductController {
     private ProductRepository productRepository;
     @GetMapping
     public ResponseEntity getAllProducts() {
-        var allProducts = productRepository.findAll();
-
+        var allProducts = productRepository.findAllByActiveTrue();
         return ResponseEntity.ok(allProducts);
     }
     @PostMapping
@@ -28,7 +30,31 @@ public class ProductController {
     }
 
     @PutMapping
+    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
-
+      Optional<Product> optionalProduct = productRepository.findById(data.id());
+      if (optionalProduct.isPresent()) {
+          Product product = optionalProduct.get();
+          product.setName(data.name());
+          product.setPrice_in_cents(data.price_in_cents());
+          return ResponseEntity.ok(product);
+      } else {
+          return ResponseEntity.notFound().build();
+      }
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setActive(false);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
--- src/main/java/com/example/crud/domain/product/RequestProduct.java
+++ src/main/java/com/example/crud/domain/product/RequestProduct.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record RequestProduct(
 
+        String id,
         @NotBlank
         String name,
         @NotNull
