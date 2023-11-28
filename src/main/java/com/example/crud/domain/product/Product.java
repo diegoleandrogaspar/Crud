94638db2e194bad039304bb4fe60d8e4c937de77
diegoleandrogaspar <diegolgaspar000@gmail.com>
2023-11-28 17:51:46 -0300

    feat: add column active

--- src/main/java/com/example/crud/domain/product/Product.java
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -19,9 +19,43 @@ public class Product {
     private String name;
     private Integer price_in_cents;
 
+    private Boolean active;
+
     public Product(RequestProduct requestProduct) {
         this.name = requestProduct.name();
         this.price_in_cents = requestProduct.price_in_cents();
+        this.active = true;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice_in_cents() {
+        return price_in_cents;
+    }
+
+    public void setPrice_in_cents(Integer price_in_cents) {
+        this.price_in_cents = price_in_cents;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
